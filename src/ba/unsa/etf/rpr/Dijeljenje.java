e020b868599bc8a17225236cea1665933e87e56e
mmujic2 <mmujic2@etf.unsa.ba>
2020-10-25 19:14:16 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Dijeljenje.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Dijeljenje {
+    public static void main(String[] args){
+        int n;
+        Scanner ulaz=new Scanner(System.in);
+        System.out.println("Unesite broj n: ");
+        n=ulaz.nextInt();
+        System.out.println("Trazeni brojevi: ");
+        for(int i=1; i<n; i++){
+            if(i % sumaCifara(i)==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int sumaCifara(int broj){
+        int suma=0;
+        do{
+            suma+=broj%10;
+            broj=broj/10;
+        }while(broj!=0);
+        return suma;
+    }
+}
